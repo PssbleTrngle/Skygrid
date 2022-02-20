ead9a40bb63d8de5b9b827060bab9b98007608bb
Niklas Widmann <niklas.widmann@zuehlke.com>
2022-02-20 12:00:32 +0100

    remove accidental web folder

--- web/src/router.tsx
+++
@@ -1,9 +0,0 @@
-import { RouteObject } from "react-router-dom";
-import Home from "./pages/Home";
-
-const routes: RouteObject[] = [
-   { path: '/', element: <Home /> },
-   { path: '*', element: <p>404 - Not Found</p> },
-]
-
-export default routes
--- web/src/util.ts
+++
@@ -1,9 +0,0 @@
-export function delay<T>(data: T, error = true) {
-   return () =>
-      new Promise<T>((res, rej) => {
-         setTimeout(() => {
-            if (error && Math.random() < 0.1) rej(new Error('an error occured'))
-            else res(data)
-         }, 100 + 300 * Math.random())
-      })
-}
