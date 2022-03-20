de2f9bcd8f487d53f678ced8f03d0002c3ebcc29
PssbleTrngle <nik@somethingcatchy.net>
2022-03-20 14:11:47 +0100

    remove datagen from web setup

--- web/util/parser/serverParser.ts
+++ web/util/parser/serverParser.ts
@@ -1,7 +1,5 @@
-import { existsSync, mkdirSync, readdirSync, readFileSync, statSync, writeFileSync } from 'fs'
-import { dirname, join, parse, resolve } from 'path'
-import { PassThrough } from 'stream'
-import { Entry } from 'unzipper'
+import { existsSync, readdirSync, readFileSync, statSync } from 'fs'
+import { join, parse, resolve } from 'path'
 import { Block } from 'util/parser/types/BlockProviders'
 import DataResolver from '../parser/DataResolver'
 import XMLParser from '../parser/XMLParser'
@@ -59,52 +57,6 @@ const translations = existsSync(langFile) ? JSON.parse(readFileSync(langFile).to
 
 export function nameOf(block: Block): string | null {
    return translations[`block.${block.mod ?? 'minecraft'}.${block.id}`] ?? null
-}
-
-interface DataMerger<T> {
-   pattern: RegExp
-   merge(a: T, b: T): T
-   path(...args: string[]): string
-}
-
-function pipeString(stream: PassThrough) {
-   return new Promise<string>((res, rej) => {
-      const chunks: Buffer[] = []
-      stream.on('data', chunk => chunks.push(Buffer.from(chunk)))
-      stream.on('error', rej)
-      stream.on('end', () => res(Buffer.concat(chunks).toString('utf8')))
-   })
-}
-
-export class Merger {
-   private mergers: DataMerger<unknown>[] = []
-
-   register<T>(merger: DataMerger<T>) {
-      this.mergers.push(merger)
-   }
-
-   async extractData(entry: Entry) {
-      await Promise.all(
-         this.mergers.map(async ({ pattern, path, merge }) => {
-            const match = entry.path.match(pattern)
-            if (match)
-               try {
-                  const content = await pipeString(entry)
-                  const resource = JSON.parse(content)
-                  const out = resolve(path(...match.slice(1)))
-                  if (existsSync(out)) {
-                     const existing = JSON.parse(readFileSync(out).toString())
-                     writeFileSync(out, JSON.stringify(merge(existing, resource), null, 2))
-                  } else {
-                     mkdirSync(dirname(out), { recursive: true })
-                     writeFileSync(out, content)
-                  }
-               } catch (e) {
-                  throw new Error(`Could not proccess ${entry.path}: ${(e as Error).message}`)
-               }
-         })
-      )
-   }
 }
 
 export default serverParser
