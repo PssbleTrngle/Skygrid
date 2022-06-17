d8ba2d77ee0ee7ad239798218e65751814dc7188
PssbleTrngle <nik@somethingcatchy.net>
2022-06-17 11:22:32 +0200

    log basePath

--- web/next.config.js
+++ web/next.config.js
@@ -1,4 +1,5 @@
 const basePath = process.env.NEXTJS_BASE_PATH || undefined
+if (basePath) console.log('Building with basePath', basePath)
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
