f7bfb5a5ba90438ed2f8624125f04c8bb51aa83c
Niklas Widmann <niklas.widmann@zuehlke.com>
2022-07-28 10:40:09 +0200

    add new cardinal extra using a random direction

--- Common/src/main/kotlin/possible_triangle/skygrid/data/xml/Extra.kt
+++ Common/src/main/kotlin/possible_triangle/skygrid/data/xml/Extra.kt
@@ -5,6 +5,7 @@ import kotlinx.serialization.Transient
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Registry
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.state.BlockState
 import possible_triangle.skygrid.data.ReferenceContext
 import possible_triangle.skygrid.data.Validating
 import possible_triangle.skygrid.util.WeightedList
@@ -24,24 +25,30 @@ abstract class Extra : Generator<IBlockAccess>, Validating {
 
     abstract fun internalValidate(blocks: Registry<Block>): Boolean
 
-    abstract fun offset(pos: BlockPos): BlockPos
+    abstract fun offset(pos: BlockPos, random: Random): BlockPos
 
     override fun validate(
         blocks: Registry<Block>,
         references: ReferenceContext,
-        additionalFilters: List<FilterOperator>
+        additionalFilters: List<FilterOperator>,
     ): Boolean {
-        validProviders = WeightedList(providers.filter { it.validate(blocks, references, ) })
+        validProviders = WeightedList(providers.filter { it.validate(blocks, references) })
         return internalValidate(blocks) && validProviders.isNotEmpty()
+    }
+
+    protected open fun transform(state: BlockState, random: Random): BlockState {
+        return state
     }
 
     override fun generate(random: Random, access: IBlockAccess): Boolean {
         val providerRandom = Random(random.nextLong())
+        val offsetRandom = random.nextLong()
 
         if (random.nextDouble() > probability) return false
-        val at = offset(BlockPos(0, 0, 0))
+        val at = offset(BlockPos(0, 0, 0), Random(offsetRandom))
 
         return validProviders.random(random).generate(providerRandom) { state, pos ->
+            val transformed = transform(state, Random(offsetRandom))
             access.set(state, pos.offset(at))
         }
     }
--- Common/src/main/kotlin/possible_triangle/skygrid/data/xml/impl/Cardinal.kt
+++ Common/src/main/kotlin/possible_triangle/skygrid/data/xml/impl/Cardinal.kt
@@ -4,28 +4,45 @@ import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 import net.minecraft.core.BlockPos
 import net.minecraft.core.Direction
+import net.minecraft.core.Direction.*
 import net.minecraft.core.Registry
 import net.minecraft.world.level.block.Block
+import net.minecraft.world.level.block.Rotation
+import net.minecraft.world.level.block.state.BlockState
 import possible_triangle.skygrid.data.xml.BlockProvider
 import possible_triangle.skygrid.data.xml.Extra
 import kotlin.random.Random
 
 @Serializable
-@SerialName("side")
-data class Side(
-    val on: String,
+@SerialName("cardinal")
+data class Cardinal(
     override val providers: List<BlockProvider>,
     val offset: Int = 1,
+    val rotate: Boolean = true,
     override val probability: Double = 1.0,
     override val shared: Boolean = false,
 ) : Extra() {
 
+    companion object {
+        val DIRECTIONS = listOf(SOUTH, WEST, NORTH, EAST, )
+    }
+
+    private fun getDirection(random: Random): Direction {
+        return DIRECTIONS.random(random)
+    }
+
     override fun internalValidate(blocks: Registry<Block>): Boolean {
-        return Direction.byName(on) != null && offset > 0
+        return offset > 0
+    }
+
+    override fun transform(state: BlockState, random: Random): BlockState {
+        val direction = getDirection(random)
+        val rotations = DIRECTIONS.indexOf(direction)
+        return state.rotate(Rotation.values()[rotations % 4])
     }
 
     override fun offset(pos: BlockPos, random: Random): BlockPos {
-        val direction = Direction.byName(on) ?: throw NullPointerException("Unknown direction '$on'")
+        val direction = getDirection(random)
         return pos.relative(direction, offset)
     }
 
