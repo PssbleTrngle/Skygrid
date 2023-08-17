ec26170228d42c7d1bb4a67e1a03e133b6f8e7cd
Niklas Widmann <nik@somethingcatchy.net>
2023-08-17 16:09:40 +0200

    remove legacy web folder

--- legacy-web/pages/api/tag.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiHandler } from 'next'
-import serverParser from 'util/parser/serverParser'
-
-const handle: NextApiHandler = async (req, res) => {
-   const matches = await serverParser.getBlocksFor(req.body)
-   res.json(matches)
-}
-
-export default handle
--- legacy-web/styles/theme/dark.ts
+++
@@ -1,9 +0,0 @@
-import { DefaultTheme } from 'styled-components'
-
-const dark: DefaultTheme = {
-   bg: '#2e2e2e',
-   text: '#EEE',
-   accent: '#6db6f2',
-}
-
-export default dark
